05fcee026dc613a02ec66efb200a27f6062fdb8c
Tobiloba David Igiebor <david.igiebor@yahoo.com>
2025-02-07 11:09:53 +0100

    Update Spinner.jsx

--- src/components/ui/Spinner.jsx
+++ src/components/ui/Spinner.jsx
@@ -1,27 +1,9 @@
-// src/components/ui/Spinner.jsx
 import React from 'react';
 
 const Spinner = () => {
     return (
-        <div className="spinner">
-            <style jsx>{`
-                .spinner {
-                    border: 4px solid rgba(0, 0, 0, 0.1);
-                    width: 36px;
-                    height: 36px;
-                    border-radius: 50%;
-                    border-left-color: #09f;
-                    animation: spin 1s ease infinite;
-                }
-                @keyframes spin {
-                    0% {
-                        transform: rotate(0deg);
-                    }
-                    100% {
-                        transform: rotate(360deg);
-                    }
-                }
-            `}</style>
+        <div className="flex justify-center items-center">
+            <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
         </div>
     );
 };
